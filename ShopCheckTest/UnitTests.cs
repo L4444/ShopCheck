2685924a121c947a54bd0e756bbef98181b09478
John C <pieboy4444@yahoo.co.uk>
2026-03-17 14:02:23 +0000

    Refactor complete

--- ShopCheckTest/UnitTests.cs
+++ ShopCheckTest/UnitTests.cs
@@ -24,29 +24,30 @@ namespace ShopCheckTest
 
 
                 ShopCheckService serve = new ShopCheckService(con);
-                Assert.IsEmpty(serve.ReadAllShopItems());
+                Assert.IsEmpty(serve.ReadAllProducts());
 
-                IList<ShopItem> inputItems = [
-                    new ShopItem { Name = "Eggs1_Too_Long_Name", MinStock = -1, MaxStock = 12, Url = "www.eggs1.com" },
-                 new ShopItem { Name = "Eggs2_Too_Long_Name", MinStock = 6, MaxStock = 12, Url = "www.eggs2.com" },
-                new ShopItem { Name = "Eggs3", MinStock = 9, MaxStock = 12, Url = "www.eggs3.com" }];
+                IList<Product> inputItems = [
+                    new Product { Name = "Eggs1_Too_Long_Name", MinStock = -1, Url = "www.eggs1-Too_Long_NameAndNegativeMinStock.com" },
+                 new Product { Name = "Eggs2_Too_Long_Name", MinStock = 0, Url = "www.eggs2-TooLongName.com" },
+                new Product { Name = "Eggs3", MinStock = 9,  Url = "www.eggs3.com" },
+                new Product { Name = "Eggs4", MinStock = -1,  Url = "www.eggs4-NegativeMinStock.com" }];
 
                 // We expect only the 3rd item to be stored (eggs3)
-                List<ShopItem> expectedItems = new List<ShopItem>();
+                List<Product> expectedItems = new List<Product>();
                 expectedItems.Add(inputItems[2]);
 
                 List<ValidationResult> result = null;
-                foreach (ShopItem si in inputItems)
+                foreach (Product si in inputItems)
                 {
                    
 
-                    result = serve.CreateNewShopItem(si);
+                    result = serve.CreateNewProduct(si);
                 }
                 Console.WriteLine(result);
 
-                IList<ShopItem> actualItems = serve.ReadAllShopItems();
+                IList<Product> actualItems = serve.ReadAllProducts();
 
-                actualItems.Should().BeEquivalentTo(expectedItems, options => options.Excluding(si => si.Id).Excluding(si => si.DateCreated));
+                actualItems.Should().BeEquivalentTo(expectedItems, options => options.Excluding(si => si.Id));
 
 
             }
@@ -67,26 +68,26 @@ namespace ShopCheckTest
 
 
                 ShopCheckService serve = new ShopCheckService(con);
-                Assert.IsEmpty(serve.ReadAllShopItems());
+                Assert.IsEmpty(serve.ReadAllProducts());
 
-                IList<ShopItem> inputItems = [
-                    new ShopItem { Name = "Eggs1", MinStock = 3, MaxStock = 12, Url = "www.eggs1.com" },
-                    new ShopItem { Name = "Eggs2", MinStock = 6, MaxStock = 12, Url = "www.eggs2.com" },
-              new ShopItem { Name = "Eggs3", MinStock = 9, MaxStock = 12, Url = "www.eggs3.com" }];
+                IList<Product> inputItems = [
+                    new Product { Name = "Eggs1", MinStock = 3,Url = "www.eggs1.com" },
+                    new Product { Name = "Eggs2", MinStock = 6,  Url = "www.eggs2.com" },
+              new Product { Name = "Eggs3", MinStock = 9,  Url = "www.eggs3.com" }];
 
                 // We expect all items to be stored
-                IList<ShopItem> expectedItems = inputItems;
+                IList<Product> expectedItems = inputItems;
 
-                foreach (ShopItem si in inputItems)
+                foreach (Product si in inputItems)
                 {
                     
                     
-                    serve.CreateNewShopItem(si);
+                    serve.CreateNewProduct(si);
                 }
 
-                IList<ShopItem> actualItems = serve.ReadAllShopItems();
+                IList<Product> actualItems = serve.ReadAllProducts();
 
-                actualItems.Should().BeEquivalentTo(expectedItems, options => options.Excluding(si => si.Id).Excluding(si => si.DateCreated));
+                actualItems.Should().BeEquivalentTo(expectedItems, options => options.Excluding(si => si.Id));
 
 
             }
