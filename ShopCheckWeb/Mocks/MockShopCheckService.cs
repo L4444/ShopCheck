c28e25c862e42358d8b4feff3f9ce4e3714b718b
John C <pieboy4444@yahoo.co.uk>
2026-03-20 18:46:15 +0000

    Implemented CRUD and My crude mocking framework should now work okay

--- ShopCheckWeb/Mocks/MockShopCheckService.cs
+++ ShopCheckWeb/Mocks/MockShopCheckService.cs
@@ -1,4 +1,5 @@
-﻿using ShopCheckDb;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using ShopCheckDb;
 
 namespace ShopCheckWeb.Mocks
 {
@@ -8,42 +9,76 @@ namespace ShopCheckWeb.Mocks
 
         public ServiceResult CreateProduct(Product product)
         {
-            Console.WriteLine($"Create New Called");
+            Console.WriteLine($"CreateProduct() Called");
+
+
+
+            ServiceResult sr = new ServiceResult();
+
+            if (product.Name != "Valid")
+            {
+
+                sr.ValidationErrors.Add("Key 1", "Validation error 1");
+                sr.ValidationErrors.Add("Key 2", "Validation error 2");
+                sr.ValidationErrors.Add("Key 3", "Validation error 3");
+                Console.WriteLine($"----- Validation failed");
+            }
+            else
+            {
+                Console.WriteLine($"----- Created new entry at Id: \'{product.Id}\'");
+             
+            }
             Console.WriteLine($"Name: \'{product.Name}\'");
             Console.WriteLine($"Url: \'{product.Url}\'");
             Console.WriteLine($"Min Stock: \'{product.MinStock}\'");
-
-            ServiceResult sr = new ServiceResult();
-            sr.ValidationErrors.Add("KEY", "VALUE");
-
             return sr;
         }
 
         public ServiceResult DeleteProduct(int id)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Deleting Product with id \'{id}\'");
+            return null;
         }
 
         public IList<Product> ReadAllProducts()
         {
             List<Product> dummyData = new List<Product>();
 
-            dummyData.Add(new Product { MinStock = 1, Name = "Bacon1", Url = "www.Bacon1.com" });
-            dummyData.Add(new Product { MinStock = 2, Name = "Bacon2", Url = "www.Bacon2.com" });
-            dummyData.Add(new Product { MinStock = 3, Name = "Bacon3", Url = "www.Bacon3.com" });
-
+            dummyData.Add(new Product {  Id = 1, MinStock = 1, Name = "Bacon1", Url = "www.Bacon1.com" });
+            dummyData.Add(new Product { Id = 2, MinStock = 2, Name = "Bacon2", Url = "www.Bacon2.com" });
+            dummyData.Add(new Product { Id = 3, MinStock = 3, Name = "Bacon3", Url = "www.Bacon3.com" });
+            
             return dummyData;
         }
 
         public Product? ReadProduct(int id)
         {
-            Console.WriteLine("Read Product Called");
-            return null;
+            
+            return new Product { Id = id, MinStock = id, Name = $"EditName{id}", Url = $"www.EditUrl{id}.com" };
         }
 
-        public ServiceResult UpdateProduct(int id, Product product)
+        public ServiceResult UpdateProduct( Product product)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("UpdateProduct() Called");
+
+            ServiceResult sr = new ServiceResult();
+            if (product.Name != "Valid")
+            {
+
+                sr.ValidationErrors.Add("Key 1", "Validation error 1");
+                sr.ValidationErrors.Add("Key 2", "Validation error 2");
+                sr.ValidationErrors.Add("Key 3", "Validation error 3");
+                Console.WriteLine($"----- Validation failed");
+            }
+            else
+            {
+                Console.WriteLine($"----- Edited entry \'{product.Id}\'");
+             
+            }
+            Console.WriteLine($"Name: \'{product.Name}\'");
+            Console.WriteLine($"Url: \'{product.Url}\'");
+            Console.WriteLine($"Min Stock: \'{product.MinStock}\'");
+            return sr;
         }
     }
 }
